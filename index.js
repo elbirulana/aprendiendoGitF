f52f7b2cb1d671a4bbf7398f01432e2f187f996c
Ulises <elbirulanaa@gmail.com>
2021-12-04 12:44:22 -0300

    tercer boceto de mi portafolio, ahora es responsivo

---
+++ index.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+    sr.reveal('.header_container',{
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.hero_img1',{
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    }); 
+
+    window.sr = ScrollReveal();
+    sr.reveal('.section',{
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    }); 
+    
+    window.sr = ScrollReveal();
+    sr.reveal('.skill',{
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    }); 
